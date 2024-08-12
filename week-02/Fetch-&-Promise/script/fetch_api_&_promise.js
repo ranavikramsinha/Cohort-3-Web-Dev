4ad4eee27d01c374ba1c4e8e604b509050ce1d15
Rana Vikram Sinha <ranavikramsinha57@gmail.com>
2024-08-12 18:30:48 +0530

    fetch api and Promise

--- week-02/Fetch-&-Promise/script/fetch_api_&_promise.js
+++ week-02/Fetch-&-Promise/script/fetch_api_&_promise.js
@@ -1,5 +1,3 @@
-
-
 //* GET is default in fetch
 // fetch('https://dummyjson.com/products')
 // .then((res) => res.json())
@@ -25,25 +23,78 @@ fetch('https://dummyjson.com/users')
     }
     return response.json();
 })
-.then((firstUser) => fetch(`https://dummyjson.com/users/${firstUser.users[0].id}`))
+.then((usersData) => fetch(`https://dummyjson.com/posts/user/${usersData.users[0].id}`))
 .then((response) => {
     if(!response.ok){
         throw new Error('Network Error');
     }
     return response.json();
 })
-.then((firstUserData) => {
-    console.log('User ID:', firstUserData.id);
-    console.log('First Name:', firstUserData.firstName);
-    console.log('Last Name:', firstUserData.lastName);
-    console.log('Email:', firstUserData.email);
-    console.log('Phone:', firstUserData.phone);
-    console.log('Address:', firstUserData.address);
-    console.log('Company:', firstUserData.company);
-
-    if(firstUserData.address) {
-        console.log('Address:', firstUserData.address.address);
-        console.log('City:', firstUserData.address.city);
-        console.log('Postal Code:', firstUserData.address.postalCode);
-      }
+.then((postsData) => fetch(`https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
+.then((response) => {
+    if(!response.ok){
+        throw new Error('Network Error');
+    }
+    return response.json();
 })
+.then((commentsData) => fetch(`https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
+.then((response) => {
+    if(!response.ok){
+        throw new Error('Network Error');
+    }
+    return response.json();
+})
+.then((userData) => console.log(userData))
+.catch((err) => console.log(err))
+
+// .then((firstUserData) => {
+//     console.log('User ID:', firstUserData.id);
+//     console.log('First Name:', firstUserData.firstName);
+//     console.log('Last Name:', firstUserData.lastName);
+//     console.log('Email:', firstUserData.email);
+//     console.log('Phone:', firstUserData.phone);
+//     console.log('Address:', firstUserData.address);
+//     console.log('Company:', firstUserData.company);
+
+//     if(firstUserData.address) {
+//         console.log('Address:', firstUserData.address.address);
+//         console.log('City:', firstUserData.address.city);
+//         console.log('Postal Code:', firstUserData.address.postalCode);
+//       }
+// })
+
+// function makeHttpRequest(method, url) {
+//     const xhr = new XMLHttpRequest()
+//     xhr.responseType = 'json'
+  
+//     const promise = new Promise((resolve, reject) => {
+//       xhr.addEventListener('load', () => {
+//         resolve(xhr.response)
+//       })
+//       xhr.addEventListener('error', () => {
+//         reject('request failed')
+//       })
+//     })
+  
+//     xhr.open(method, url)
+//     xhr.send()
+  
+//     return promise
+//   }
+  
+//   // makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
+//   //   makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
+//   //     makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
+//   //       makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
+//   //         console.log(userData);
+//   //       });
+//   //     });
+//   //   });
+//   // })
+  
+//   makeHttpRequest('GET', 'https://dummyjson.com/users')
+//     .then((usersData) => makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`))
+//     .then((postsData) => makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`))
+//     .then((commentsData) => makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`))
+//     .then((userData) => console.log(userData))
+//     .catch((err) => console.log(err))
